00ced52ec75b7698963589102789fe57c7080e9c
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-12-29 17:16:35 +0100

    Repository code

--- Repository/ICityService.cs
+++ Repository/ICityService.cs
@@ -1,7 +1,15 @@
-namespace BlazorAppSPA.Repository
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorAppSPA.Models;
+
+namespace BlazorDapperSPA.Data
 {
     public interface ICityService
     {
-         
+        Task<IEnumerable<City>> GetCities();
+        Task<bool> CreateCity(City city);
+        Task<bool> EditCity(int id, City city);
+        Task<City> SingleCity(int id);
+        Task<bool> DeleteCity(int id);
     }
 }
--- Repository/IEmployeeService.cs
+++ Repository/IEmployeeService.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorAppSPA.DTO;
+using BlazorAppSPA.Models;
+
 namespace BlazorAppSPA.Repository
 {
-    public interface IEmployeeService
+public interface IEmployeeService
     {
-         
+        Task<IEnumerable<EmployeeModel>> GetEmployees();
+        Task<bool> CreateEmployee(Employee employee);
+        Task<bool> EditEmployee(int id, EmployeeModel employee);
+        Task<EmployeeModel> SingleEmployee(int id);
+        Task<bool> DeleteEmployee(int id);
     }
+
 }
